90ff8238a79f05abb2ac73f4084d49e9239af222
Le Pham <phamhongle412004@gmail.com>
2026-07-03 09:49:00 +0700

    Tạo lớp dữ liệu OrderRepository

--- src/main/java/com/stockpilot/exception/InsufficientStockException.java
+++ src/main/java/com/stockpilot/exception/InsufficientStockException.java
@@ -1,2 +1,7 @@
-package com.stockpilot.exception;public class InsufficientStockException {
+package com.stockpilot.exception;
+
+public class InsufficientStockException extends RuntimeException {
+    public InsufficientStockException(String message) {
+        super(message);
+    }
 }
